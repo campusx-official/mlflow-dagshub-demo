37b3e2dade98a7601b499519dd4ddffcef1158ed
nitishksingh24 <nitishksingh24@gmail.com>
2024-07-13 13:33:06 +0530

    add dagshub config code

--- iris-dt.py
+++ iris-dt.py
@@ -7,7 +7,10 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+import dagshub
+dagshub.init(repo_owner='campusx-official', repo_name='mlflow-dagshub-demo', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/campusx-official/mlflow-dagshub-demo.mlflow")
 
 # Load the iris dataset
 iris = load_iris()
